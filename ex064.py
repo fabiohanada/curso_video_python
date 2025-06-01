bedf5db11a4c0b2dcd20487271307eede9179244
fabiohanada23 <61828534+fabiohanada23@users.noreply.github.com>
2025-06-01 08:06:37 -0300

    ex065

--- ex064.py
+++ ex064.py
@@ -1,12 +1,21 @@
-cont = 0
+'''cont = 0
 soma = 0
 while cont != 999:
     n = int(input('Digite um numero [999 para parar]: '))
     if n != 999:
-
         cont += 1
         soma += n
     else:
         print('Voce digitou {} numeros e a soma entre eles foi {}'.format(cont, soma))
-        break
+        break'''
+
+num = cont = soma = 0
+num = int(input('Digite um numero [999 para parar]: '))
+while num != 999:
+    soma += num
+    cont += 1
+    num = int(input('Digite um numero [999 para parar]: '))
+print('Voce digitou {} numeros e a soma entre eles foi {}.'.format(cont, soma))
+
+
 
---
+++ ex065.py
@@ -0,0 +1,22 @@
+cont = 0
+soma = 0
+maior = 0
+menor = 99
+while cont != 999:
+    n = int(input("Digite um numero: "))
+    parada = str(input('Deseja continuar? [S/N] '))
+    cont += 1
+    soma += n
+    if n > maior:
+        maior = n
+    if n < menor:
+        menor = n
+    if parada == 'n':
+        media = soma / cont
+        print('Voce digitou {} e a media foi {}'.format(cont, media))
+        print(maior)
+        print(menor)
+        break
+
+
+
