a0f75d798ee098c95909cee63e7ff9c88418023a
fabiohanada23 <61828534+fabiohanada23@users.noreply.github.com>
2025-06-29 08:00:29 -0300

    ex084

--- ex083.py
+++ ex083.py
@@ -1,8 +1,24 @@
-expressao = input('Digite a expressão: ')
+'''expressao = input('Digite a expressão: ')
 quantidade_parenteses_abertura = expressao.count('(')
 quantidade_parenteses_fechamento = expressao.count(')')
 
 if quantidade_parenteses_abertura == quantidade_parenteses_fechamento:
     print('Sua expressão esta valida!')
 else:
-    print('Sua expressão esta errada!')
+    print('Sua expressão esta errada!')'''
+
+expr = str(input('Digite a expressao: '))
+pilha = []
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está valida!')
+else:
+    print('Sua expressão esta errada')
---
+++ ex084.py
@@ -0,0 +1,65 @@
+'''pessoa = list()
+dado = list()
+pessoas = 0
+maior = 0
+menor = 999
+while True:
+    dado.append(str(input('Nome: ')))
+    dado.append((float(input('Peso: '))))
+    pessoa.append(dado[:])
+    dado.clear()
+    pessoas += 1
+
+    resp = str(input('Quer continuar: [S/N] '))
+    if resp in 'Nn':
+        break
+
+for p in pessoa:
+    if p[1] >= maior:
+        maior = p[1]
+    elif p[1] <= menor:
+        menor = p[1]
+
+print(f'Ao todo, voce cadastrou {pessoas} pessoas. ')
+print(f'O maior peso foi de {maior}Kg. Peso de ', end='')
+for a in pessoa:
+    if a[1] == maior:
+        print(f'[{a[0]}] ', end='')
+print()
+print(f'O menor peso foi de {menor}Kg. Peso de ', end='')
+for b in pessoa:
+    if b[1] == menor:
+        print(f'[{b[0]}] ', end='')'''
+
+temp = []
+princ = []
+mai = men = 0
+while True:
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso: ')))
+    if len(princ) == 0:
+        mai = men = temp[1]
+    else:
+        if temp[1] > mai:
+            mai = temp[1]
+        if temp[1] < men:
+            men = temp[1]
+    princ.append(temp[:])
+    temp.clear()
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+print('-=' * 30)
+print(f'Ao, todo, voce cadastrou {len(princ)} pessoas.')
+print(f'O maior peso foi de {mai}Kg. Peso de ', end='')
+for p in princ:
+    if p[1] == mai:
+        print(f'[{p[0]}] ', end='')
+print()
+print(f'O menor peso foi de {men}Kg. Peso de ', end='')
+for p in princ:
+    if p[1] == men:
+        print(f'[{p[0]}] ', end='')
+print()
+
+
